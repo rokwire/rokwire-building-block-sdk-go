edc6eea4b42aa19a28e63462ecbca1c92ba8bbc1
Stefan Vitanov <stefan@inabyte.com>
2024-10-29 11:26:28 +0200

    set get groups membership by group id

--- services/groups/services.go
+++ services/groups/services.go
@@ -84,6 +84,48 @@ func (na *GroupsAdapter) GetGroupMemberships(logs logs.Logger, userID string) ([
 	return getGroupMemberships, nil
 }
 
+// GetGroupMembershipsByGroupID Get group memebers by groupID
+func (na *GroupsAdapter) GetGroupMembershipsByGroupID(logs logs.Logger, groupID string) ([]string, error) {
+	url := fmt.Sprintf("%s/api/bbs/groups/%s/group-memberships", na.groupsBaseURL, groupID)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		logs.Errorf("GetGroupMembershipsByGroupID:error creating load user data request - %s", err)
+		return nil, err
+	}
+
+	resp, err := na.serviceAccountManager.MakeRequest(req, "all", "all")
+	if err != nil {
+		logs.Errorf("GetGroupMembershipsByGroupID: error sending request - %s", err)
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		errorResponse, _ := ioutil.ReadAll(resp.Body)
+		if errorResponse != nil {
+			logs.Errorf("GetGroupMembershipsByGroupID: error with response code - %s", errorResponse)
+		}
+		logs.Errorf("GetGroupMembershipsByGroupID: error with response code - %d", resp.StatusCode)
+		return nil, fmt.Errorf("GetGroupMembershipsByGroupID:error with response code != 200")
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("GetGroupMembershipsByGroupID: unable to read json: %s", err)
+		return nil, fmt.Errorf("GetGroupMembershipsByGroupID: unable to parse json: %s", err)
+	}
+
+	var groupMembers []string
+	err = json.Unmarshal(data, &groupMembers)
+	if err != nil {
+		log.Printf("GetGroupMembershipsByGroupID: unable to parse json: %s", err)
+		return nil, fmt.Errorf("GetGroupMembershipsByGroupID: unable to parse json: %s", err)
+	}
+
+	return groupMembers, nil
+}
+
 // NewGroupsService creates and configures a new Service instance
 func NewGroupsService(serviceAccountManager *auth.ServiceAccountManager, groupsBaseURL string, logger *logs.Logger) (*GroupsAdapter, error) {
 	if serviceAccountManager == nil {
