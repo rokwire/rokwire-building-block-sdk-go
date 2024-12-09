b820d1bbb76d98f9a2cfdc4d6ce79a14397cb055
Stefan Vitanov <stefan@inabyte.com>
2024-12-09 10:20:41 +0200

    add GetUserAccount fin the CoreBB Adapter

--- services/core/service.go
+++ services/core/service.go
@@ -15,9 +15,12 @@
 package core
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"time"
 
@@ -112,6 +115,88 @@ func (c *Service) buildDeletedAccountsRequest() (*http.Request, error) {
 	return req, nil
 }
 
+// GetUserAccounts Gets user accounts
+func (c *Service) GetUserAccounts(searchParams map[string]interface{}, appID *string, orgID *string, limit *int, offset *int) ([]CoreAccountResponse, error) {
+	if c.serviceAccountManager == nil {
+		log.Println("GetAccounts: service account manager is nil")
+		return nil, errors.New("service account manager is nil")
+	}
+
+	url := fmt.Sprintf("%s/bbs/accounts", c.serviceAccountManager.AuthService.AuthBaseURL)
+
+	bodyBytes, err := json.Marshal(searchParams)
+	if err != nil {
+		log.Printf("GetAccounts: error marshalling body - %s", err)
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewReader(bodyBytes))
+	if err != nil {
+		log.Printf("GetAccounts: error creating request - %s", err)
+		return nil, err
+	}
+
+	params := req.URL.Query()
+	if appID != nil {
+		params.Add("app_id", *appID)
+	}
+	if orgID != nil {
+		params.Add("org_id", *orgID)
+	}
+	if limit != nil {
+		params.Add("limit", fmt.Sprintf("%d", *limit))
+	}
+	if offset != nil {
+		params.Add("offset", fmt.Sprintf("%d", *offset))
+	}
+	req.URL.RawQuery = params.Encode()
+
+	req.Header.Add("Content-Type", "application/json")
+
+	appIDVal := "all"
+	if appID != nil {
+		appIDVal = *appID
+	}
+	orgIDVal := "all"
+	if orgID != nil {
+		appIDVal = *orgID
+	}
+	resp, err := c.serviceAccountManager.MakeRequest(req, appIDVal, orgIDVal)
+	if err != nil {
+		log.Printf("GetAccounts: error sending request - %s", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		log.Printf("GetAccounts: error with response code - %d", resp.StatusCode)
+		return nil, fmt.Errorf("GetAccounts: error with response code != 200")
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("GetAccounts: unable to read json: %s", err)
+		return nil, fmt.Errorf("GetAccounts: unable to parse json: %s", err)
+	}
+
+	var maping []CoreAccount
+	err = json.Unmarshal(data, &maping)
+	if err != nil {
+		log.Printf("GetAccounts: unable to parse json: %s", err)
+		return nil, fmt.Errorf("GetAccounts: unable to parse json: %s", err)
+	}
+
+	var coreAccounts []CoreAccountResponse
+	for _, ca := range maping {
+		if ca.ID != "" {
+			cat := CoreAccountResponse{ID: ca.ID, Name: ca.Profile.FirstName}
+			coreAccounts = append(coreAccounts, cat)
+		}
+
+	}
+
+	return coreAccounts, nil
+}
+
 // NewService creates and configures a new Service instance
 func NewService(serviceAccountManager *auth.ServiceAccountManager, deletedAccountsConfig *DeletedAccountsConfig, logger *logs.Logger) (*Service, error) {
 	if serviceAccountManager == nil {
@@ -133,7 +218,6 @@ func NewService(serviceAccountManager *auth.ServiceAccountManager, deletedAccoun
 	}
 
 	core := Service{serviceAccountManager: serviceAccountManager, deletedAccountsConfig: deletedAccountsConfig, logger: logger}
-
 	return &core, nil
 }
 
@@ -145,4 +229,50 @@ type DeletedAccountsConfig struct {
 	path      string
 	timerDone chan bool
 	timer     *time.Timer
+}
+
+// CoreAccount wraps the account structure from the Core BB
+// @name CoreAccount
+type CoreAccount struct {
+	AuthTypes []struct {
+		Active       bool   `json:"active"`
+		AuthTypeCode string `json:"auth_type_code"`
+		AuthTypeID   string `json:"auth_type_id"`
+		Identifier   string `json:"identifier"`
+		Params       struct {
+			User struct {
+				Email          string        `json:"email"`
+				FirstName      string        `json:"first_name"`
+				Groups         []interface{} `json:"groups"`
+				Identifier     string        `json:"identifier"`
+				LastName       string        `json:"last_name"`
+				MiddleName     string        `json:"middle_name"`
+				Roles          []string      `json:"roles"`
+				SystemSpecific struct {
+					PreferredUsername string `json:"preferred_username"`
+				} `json:"system_specific"`
+			} `json:"user"`
+		} `json:"params"`
+	} `json:"auth_types"`
+	Profile struct {
+		Address   string `json:"address"`
+		BirthYear int    `json:"birth_year"`
+		Country   string `json:"country"`
+		Email     string `json:"email"`
+		FirstName string `json:"first_name"`
+		ID        string `json:"id"`
+		LastName  string `json:"last_name"`
+		Phone     string `json:"phone"`
+		PhotoURL  string `json:"photo_url"`
+		State     string `json:"state"`
+		ZipCode   string `json:"zip_code"`
+	} `json:"profile"`
+	ID string `json:"id"`
+}
+
+// CoreAccount wraps the accountID and first name of the account
+// @name CoreAccountResponse
+type CoreAccountResponse struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
 }
